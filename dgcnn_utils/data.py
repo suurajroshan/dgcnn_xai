e514701d1f0e91316667c3d16606e0d333840485
suurajroshan <suuraj.perpeli@fau.de>
2024-07-20 18:34:37 +0200

    remove files of unused models

--- dgcnn_utils/data.py
+++ dgcnn_utils/data.py
@@ -6,7 +6,6 @@ import numpy as np
 import torch
 import json
 import cv2
-import pickle
 from torch.utils.data import Dataset
 
 
@@ -18,16 +17,6 @@ def download_S3DIS():
     if not os.path.exists(os.path.join(DATA_DIR, "indoor3d_sem_seg_hdf5_data")):
         print("No data avaialable")
         sys.exit(0)
-    # if not os.path.exists(os.path.join(DATA_DIR, 'Stanford3dDataset_v1.2_Aligned_Version')):
-    #     if not os.path.exists(os.path.join(DATA_DIR, 'Stanford3dDataset_v1.2_Aligned_Version.zip')):
-    #         print('Please download Stanford3dDataset_v1.2_Aligned_Version.zip \
-    #             from https://goo.gl/forms/4SoGp4KtH1jfRqEj2 and place it under data/')
-    #         sys.exit(0)
-    # else:
-    #     zippath = os.path.join(DATA_DIR, 'Stanford3dDataset_v1.2_Aligned_Version.zip')
-    #     os.system('unzip %s' % (zippath))
-    #     os.system('mv %s %s' % ('Stanford3dDataset_v1.2_Aligned_Version', DATA_DIR))
-    #     os.system('rm %s' % (zippath))
 
 
 def prepare_test_data_semseg():
@@ -77,12 +66,11 @@ def load_data_semseg(partition, test_area):
     return all_data, all_seg
 
 
-def read_txt_file(file_path, num_points: np.int32):
+def read_txt_file(file_path):
     data_app = []
     labels_app = []
     for f in file_path:
         with open(f) as file:
-            count = 0
             data = []
             labels = []
             for line in file:
@@ -135,155 +123,6 @@ def load_data_file(data_path, stage_name):
     data = f[str(stage_name)]["data"][:]
     label = f[str(stage_name)]["labels"][:]
     return (data, label)
-
-
-def load_color_partseg():
-    colors = []
-    labels = []
-    f = open("prepare_data/meta/partseg_colors.txt")
-    for line in json.load(f):
-        colors.append(line["color"])
-        labels.append(line["label"])
-    partseg_colors = np.array(colors)
-    partseg_colors = partseg_colors[:, [2, 1, 0]]
-    partseg_labels = np.array(labels)
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    img_size = 1350
-    img = np.zeros((1350, 1890, 3), dtype="uint8")
-    cv2.rectangle(img, (0, 0), (1900, 1900), [255, 255, 255], thickness=-1)
-    column_numbers = [4, 2, 2, 4, 4, 3, 3, 2, 4, 2, 6, 2, 3, 3, 3, 3]
-    column_gaps = [320, 320, 300, 300, 285, 285]
-    color_size = 64
-    color_index = 0
-    label_index = 0
-    row_index = 16
-    for row in range(0, img_size):
-        column_index = 32
-        for column in range(0, img_size):
-            color = partseg_colors[color_index]
-            label = partseg_labels[label_index]
-            length = len(str(label))
-            cv2.rectangle(
-                img,
-                (column_index, row_index),
-                (column_index + color_size, row_index + color_size),
-                color=(int(color[0]), int(color[1]), int(color[2])),
-                thickness=-1,
-            )
-            img = cv2.putText(
-                img,
-                label,
-                (
-                    column_index + int(color_size * 1.15),
-                    row_index + int(color_size / 2),
-                ),
-                font,
-                0.76,
-                (0, 0, 0),
-                2,
-            )
-            column_index = column_index + column_gaps[column]
-            color_index = color_index + 1
-            label_index = label_index + 1
-            if color_index >= 50:
-                cv2.imwrite(
-                    "prepare_data/meta/partseg_colors.png",
-                    img,
-                    [cv2.IMWRITE_PNG_COMPRESSION, 0],
-                )
-                return np.array(colors)
-            elif column + 1 >= column_numbers[row]:
-                break
-        row_index = row_index + int(color_size * 1.3)
-        if row_index >= img_size:
-            break
-
-
-def load_color_semseg():
-    colors = []
-    labels = []
-    f = open("dgcnn_pytorch/prepare_data/meta/semseg_colors.txt")
-    for line in json.load(f):
-        colors.append(line["color"])
-        labels.append(line["label"])
-    semseg_colors = np.array(colors)
-    semseg_colors = semseg_colors[:, [2, 1, 0]]
-    partseg_labels = np.array(labels)
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    img_size = 1500
-    img = np.zeros((500, img_size, 3), dtype="uint8")
-    cv2.rectangle(img, (0, 0), (img_size, 750), [255, 255, 255], thickness=-1)
-    color_size = 64
-    color_index = 0
-    label_index = 0
-    row_index = 16
-    for _ in range(0, img_size):
-        column_index = 32
-        for _ in range(0, img_size):
-            color = semseg_colors[color_index]
-            label = partseg_labels[label_index]
-            length = len(str(label))
-            cv2.rectangle(
-                img,
-                (column_index, row_index),
-                (column_index + color_size, row_index + color_size),
-                color=(int(color[0]), int(color[1]), int(color[2])),
-                thickness=-1,
-            )
-            img = cv2.putText(
-                img,
-                label,
-                (
-                    column_index + int(color_size * 1.15),
-                    row_index + int(color_size / 2),
-                ),
-                font,
-                0.7,
-                (0, 0, 0),
-                2,
-            )
-            column_index = column_index + 200
-            color_index = color_index + 1
-            label_index = label_index + 1
-            if color_index >= 13:
-                cv2.imwrite(
-                    "prepare_data/meta/semseg_colors.png",
-                    img,
-                    [cv2.IMWRITE_PNG_COMPRESSION, 0],
-                )
-                return np.array(colors)
-            elif column_index >= 1280:
-                break
-        row_index = row_index + int(color_size * 1.3)
-        if row_index >= img_size:
-            break
-
-
-def translate_pointcloud(pointcloud):
-    xyz1 = np.random.uniform(low=2.0 / 3.0, high=3.0 / 2.0, size=[3])
-    xyz2 = np.random.uniform(low=-0.2, high=0.2, size=[3])
-
-    translated_pointcloud = np.add(np.multiply(pointcloud, xyz1), xyz2).astype(
-        "float32"
-    )
-    return translated_pointcloud
-
-
-def jitter_pointcloud(pointcloud, sigma=0.01, clip=0.02):
-    N, C = pointcloud.shape
-    pointcloud += np.clip(sigma * np.random.randn(N, C), -1 * clip, clip)
-    return pointcloud
-
-
-def rotate_pointcloud(pointcloud):
-    theta = np.pi * 2 * np.random.uniform()
-    rotation_matrix = np.array(
-        [[np.cos(theta), -np.sin(theta)], [np.sin(theta), np.cos(theta)]]
-    )
-    pointcloud[:, [0, 2]] = pointcloud[:, [0, 2]].dot(
-        rotation_matrix
-    )  # random rotation (x,z)
-    return pointcloud
 
 
 class S3DIS(Dataset):
